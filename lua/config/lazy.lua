c48df8be07e6d83a9d36197a6fe3abd348266ff4
ManuaL <aniket1mujumdar@gmail.com>
2026-02-28 12:49:21 +0530

    Add fzf extra on clean install

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -18,6 +18,8 @@ require("lazy").setup({
   spec = {
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
+    -- Add some extras needed
+    { import = "lazyvim.plugins.extras.editor.fzf" },
     -- import/override with your plugins
     { import = "plugins" },
   },
